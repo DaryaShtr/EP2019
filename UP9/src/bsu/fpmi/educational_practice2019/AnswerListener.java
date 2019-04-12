626cfd263fca134a1a9cf2a4d5c8dae0843b3c87
DaryaShtr <dasasht@mail.ru>
2019-04-12 21:54:00 +0300

    the nineth lab

---
+++ UP9/src/bsu/fpmi/educational_practice2019/AnswerListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bsu.fpmi.educational_practice2019;
+
+/**
+ *
+ * @author Даша
+ */
+public interface AnswerListener extends java.util.EventListener {
+    public void Result(AnswerEvent e);
+}
---
+++ UP9/src/bsu/fpmi/educational_practice2019/MyLabelEditor.java
@@ -0,0 +1,30 @@
+
+package bsu.fpmi.educational_practice2019;
+
+import java.beans.PropertyEditorSupport;
+import javax.swing.JLabel;
+
+public class MyLabelEditor extends PropertyEditorSupport {
+	
+	@Override
+	public String[] getTags() {
+		return new String[] {"MyLabel"};
+	}
+	
+	@Override
+	public void setAsText(String s) {
+		setValue(new JLabel(s));
+	}
+
+	@Override 
+	public String getAsText() {
+		return ((JLabel)getValue()).getText();
+	}
+
+	@Override
+	public String getJavaInitializationString() {
+		return "";
+	}
+	
+}
+//
